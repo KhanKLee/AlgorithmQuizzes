304133a4279d4fda7546078ab9d243514e762b05
KhanKLee <100663958+KhanKLee@users.noreply.github.com>
2022-10-05 20:46:26 -0400

    Add files via upload

---
+++ CharBomb.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class CharBomb {
+
+	public static void main(String [] args)
+	{
+		Scanner input = new Scanner(System.in);
+		
+		String firstLine = input.nextLine();
+		
+		String secondLine = input.nextLine();
+		
+		
+		char[] ary1 = new char[firstLine.length()];
+		char[] ary2 = new char[secondLine.length()];
+		String result = "";
+		boolean same = false;
+		
+		for(int i = 0; i < ary1.length; i++)
+		{
+			ary1[i] = firstLine.charAt(i);
+		}
+		
+		for(int i = 0; i < ary2.length; i++)
+		{
+			ary2[i] = secondLine.charAt(i);
+		}
+		
+		for(int i = 0; i < ary1.length; i++)
+		{
+			for(int j = 0; j < ary2.length; j++)
+			{
+				if(ary1[i] == ary2[j])
+				{
+					same = true;
+				}	
+			}
+			
+			if(!same)
+			{
+				result += ary1[i];
+			}
+			
+			same = false;
+		}
+		
+		if(result == "")
+		{
+			System.out.println("FRULA");
+		}
+		else
+		{
+			System.out.println(result);
+		}
+		
+		
+		
+	}
+}
